abd33191bc07dc340c2499d40a653fba6b532c62
lmtoo <601235723@qq.com>
2018-06-21 00:13:45 +0800

    新增ribbon负载均衡

--- demo/src/main/java/com/example/demo/web/HelloController.java
+++ demo/src/main/java/com/example/demo/web/HelloController.java
@@ -1,25 +1,30 @@
 package com.example.demo.web;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-
-
-
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/index")
-    public String index(){
+    public String index() {
+        ServiceInstance instance = discoveryClient.getInstances("demo-service").get(0);
+
+        logger.info("/hello, host: " + instance.getHost() + ", service_id:" + instance.getServiceId());
+
         return "Hello world";
     }
-
-
-
-
 
 
 }
